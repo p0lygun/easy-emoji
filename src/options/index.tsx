ded2a79ba7727b3d20ad1e94f9e7e03e9ba1e0fd
p0lygun <solankivibhakar82@gmail.com>
2023-12-08 20:08:17 +0530

    feat(core): use template folders

---
+++ src/options/index.tsx
@@ -0,0 +1,10 @@
+
+
+import "style.css"
+
+function IndexOptions() {
+
+  return <div className="text-slate-300 text-xl"></div>
+}
+
+export default IndexOptions
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,12 +1,18 @@
+import emojis from "emojilib"
 import "style.css"
+
 
 function IndexOptions() {
   
+    const emoji_keys = Object.keys(emojis),
+        random_emoji = emoji_keys[emoji_keys.length*Math.random() << 0];
     return (
-    <div className="bg-slate-900 flex flex-col px-2 py-1  w-72 text-slate-300 gap-y-3 text-sm"> 
-        <div className="font-bold">See options page to change identifier</div>
-        <div className="flex flex-col">
+    <div className="bg-slate-900 flex flex-col items-center w-72 text-slate-300 mt-2 gap-y-1 text-sm"> 
+        {/* <div className="font-bold px-2">See options page to change identifier</div> */}
+        <div className="flex flex-col bg-slate-800 items-center text-xs w-full pt-2">
             <span>Made with ❤️ by <a className="underline" href="https://github.com/p0lygun" target="_blank">gala_vs</a></span>
+            <div className="text-[0.6]">{emoji_keys.length} emojis available. {random_emoji}</div>
+            
             <span className="text-[0.5rem] flex flex-row gap-x-1">
                     <span>Support the project at</span>
                     <a href="https://github.com/p0lygun/easy-emoji" target="_blank" className="text-pink-500 font-bold">Github</a>
